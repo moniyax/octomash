b6178a8d741677efcffe8bce7137df606588d3e8
Mayoa Yakob <mayoa.net@gmail.com>
2013-07-29 01:01:19 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MooOrMoo::Application.config.secret_key_base = '291decd11b2e62b4c9e72d2fa11e434edeba051c23d39b9740fcc974d3ad2fd214393ac0092cfeec29f11ba4863b0364d23975b2e6aef325c6c733db6367a327'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MooOrMoo::Application.config.session_store :cookie_store, key: '_moo_or_moo_session'
